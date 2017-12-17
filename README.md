2503849b0f140e19e5877bebff613f8540602a55
zbianca <bianca.zanardi@gmail.com>
2017-12-17 23:47:26 +0100

    feat: second color and export image. docs: listed requirements and added links to original files.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# pixel-art-maker
-JQuery Project
+# Pixel Art Maker
+### JQuery Project for Udacity frontend course
+
+Users should be able to:
+1. Dynamically set the size of the table as an N by M grid.
+1. Choose a color.
+1. Click a cell in the grid to fill that cell with the chosen color.
+
+Extra functionalities I included:
+- Hide gridlines.
+- Keep painting on click and drag.
+- Choose a second color on right-click.
+- Export image as .png.
+
+### Resources
+- [Udacity's original repository](https://github.com/udacity/project-pixel-art-maker-starterdw.de/)
+- [Udacity's style guide](http://udacity.github.io/frontend-nanodegree-styleguide/index.html)
--- designs.js
+++ designs.js
@@ -1,23 +1,52 @@
-let isDown = false; // Tracks mouse button
+let isDown = false; // Track mouse button
+let pixelData = [];
 
-function makeGrid(cols, rows) {
-    let addTr = '<tr></tr>'.repeat(cols);
-    let addTd = '<td></td>'.repeat(rows);
-    $('#pixel_canvas')
-        .empty()
-        .append(addTr);
-    $('#pixel_canvas tr').append(addTd);
-    $('#show-grid').prop('checked', true); // Fixes gridlines toggle-label incosistency
-    $('#gridlines').removeClass('hidden');
-    isDown = false; // Mouse up
+// Generate table and add rows-arrays to pixelData
+function makeGrid(rows, cols) {
+    $('#pixel_canvas').empty();
+    for (var row = 0; row < rows; row++) {
+        let addTr = '<tr>';
+        for (var col = 0; col < cols; col++) {
+            addTr += '<td data-x="' + row + '" data-y="' + col + '"></td>';
+        }
+        addTr += '</tr>';
+        $('#pixel_canvas').append(addTr);
+    }
+    for (let i = 0; i < rows; i++) {
+        pixelData[i] = [];
+    }
+    resetGrid();
 }
 
+function resetGrid() {
+    $('#show-grid').prop('checked', true); // Fix gridlines toggle-label incosistency
+    $('#gridlines').removeClass('hidden');
+    $('#download').removeClass('hidden');
+}
+
+// Paint table and add hex value to pixelData
 function paint(td, color) {
     $(td).css('background-color', color.val());
+    let canvasX = parseInt($(td).data('x'));
+    let canvasY = parseInt($(td).data('y'));
+    pixelData[canvasX][canvasY] = color.val();
 }
 
-$(function () { //document is ready
-    const color = $('#colorPicker');
+function hextoRGBA(hex) {
+    if (hex) {
+        let r = parseInt(hex.slice(1, 3), 16);
+        let g = parseInt(hex.slice(3, 5), 16);
+        let b = parseInt(hex.slice(5, 7), 16);
+        return "rgba(" + r + ", " + g + ", " + b + ", 1)";
+    } else {
+        return "rgba(255, 255, 255, 1)";
+    }
+}
+
+$(function () { // Document is ready
+
+    const color1 = $('#colorPicker1');
+    const color2 = $('#colorPicker2');
     const height = $('#input_height');
     const width = $('#input_width');
 
@@ -27,28 +56,59 @@ $(function () { //document is ready
         makeGrid(height.val(), width.val());
     });
 
-    // On table cell click or click and drag, call paint()
+    // On table cell click or click && drag, call paint()
     $('#pixel_canvas').mousedown('td', function (event) {
             event.preventDefault();
             isDown = true; // Mouse down
-            let td = event.target.closest('td');
-            paint(td, color);
-        })
-        .mouseup(function () {
-            isDown = false; // Mouse up
+            let cell = event.target.closest('td');
+            let color = event.which === 1 ? color1 : color2;
+            paint(cell, color);
         })
         .mouseover('td', function (event) {
-            let td = event.target.closest('td');
+            let cell = event.target.closest('td');
+            let color = event.which === 1 ? color1 : color2;
             if (isDown) {
-                paint(td, color);
+                paint(cell, color);
             }
         })
-        .mouseleave(function () {
-            isDown = false; // Mouse up outside canvas
+        .on("mouseup mouseleave", function () {
+            isDown = false; // Mouse up or outside canvas
+        })
+        .contextmenu(function () {
+            return false; // Disabled inside canvas
         });
 
     // Toggle gridlines
     $('#show-grid').change(function () {
         $('#pixel_canvas tr, #pixel_canvas td').toggleClass('no-border');
     });
+
+    var setCanvas = function(w, h) {
+        $('#canvas').attr("width", w);
+        $('#canvas').attr("height", h)
+    };
+
+    var dataToCanvas = function(w, h) {
+        let canvas = document.getElementById('canvas');
+        $ctx = canvas.getContext('2d');
+        $ctx.clearRect(0, 0, w, h);
+        for (var x = 0; x < h; x++) {
+            for (var y = 0; y < w; y++) {
+                let hex = pixelData[x][y];
+                $ctx.fillStyle = hextoRGBA(hex);
+                $ctx.fillRect(y, x, 1, 1);
+            }
+        }
+    };
+
+    $('#export').click(function (event) {
+        let cwidth = parseInt(width.val(), 10);
+        let cheight = parseInt(height.val(), 10);
+        setCanvas(cwidth, cheight);
+        dataToCanvas(cwidth, cheight);
+        const link = document.getElementById('download');
+        link.href = canvas.toDataURL(); // Set data-uri as href, default PNG
+        link.download = 'myPixelArt.png'; // Set filename
+    });
+
 });
